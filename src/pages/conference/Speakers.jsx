3d0477331e2d0886d47bda62a10fd2e3112e2586
iranjith <rgranjith89@gmail.com>
2024-04-28 18:34:28 +0530

    feat: query to retrieve speakers

--- src/pages/conference/Speakers.jsx
+++ src/pages/conference/Speakers.jsx
@@ -1,71 +1,103 @@
 import * as React from "react";
 import "./style-sessions.css";
-
+import { gql, useQuery } from "@apollo/client";
 /* ---> Define queries, mutations and fragments here */
 
-const SpeakerList = () => {
+const SPEAKERS = gql`
+  query speakers {
+    speakers {
+      id
+      name
+      bio
+      sessions {
+        id
+        title
+        day
+        level
+        room
+        startsAt
+      }
+    }
+  }
+`;
 
-  /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+const SpeakerList = () => {
+  const { loading, error, data } = useQuery(SPEAKERS);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error loading speakers: {error.message}</p>;
+
   const featured = false;
 
-  return (
-		<div
-      key={'id'}
+  return data.speakers.map(({ id, name, bio, sessions }) => (
+    <div
+      key={"id"}
       className="col-xs-12 col-sm-6 col-md-6"
       style={{ padding: 5 }}
     >
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'Speaker: '}</h3>
+          <h3 className="panel-title">{`Speaker: ${name}`}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'Bio: ' }</h5>
+          <h5>{`Bio: ${bio}`}</h5>
         </div>
         <div className="panel-footer">
           <h4>Sessions</h4>
-					{
-						/* ---> Loop through speaker's sessions here */
-					}
-          <span>	
-            <button	
-              type="button"	
-              className="btn btn-default btn-lg"	
-              onClick={()=> {
+          {
+            /* ---> Loop through speaker's sessions here */
+            sessions.map((session) => (
+              <span key={session.id}>
+                <h5>{`Title: ${session.title}`}</h5>
+                <h5>{`Day: ${session.day}`}</h5>
+                <h5>{`Level: ${session.level}`}</h5>
+                <h5>{`Room: ${session.room}`}</h5>
+                <h5>{`Starts at: ${session.startsAt}`}</h5>
+              </span>
+            ))
+          }
+          <span>
+            <button
+              type="button"
+              className="btn btn-default btn-lg"
+              onClick={() => {
                 /* ---> Call useMutation's mutate function to mark speaker as featured */
-              }}	
-              >	
-                <i	
-                  className={`fa ${featured ? "fa-star" : "fa-star-o"}`}	
-                  aria-hidden="true"	
-                  style={{	
-                    color: featured ? "gold" : undefined,	
-                  }}	
-                ></i>{" "}	
-                Featured Speaker	
-            </button>	
+              }}
+            >
+              <i
+                className={`fa ${featured ? "fa-star" : "fa-star-o"}`}
+                aria-hidden="true"
+                style={{
+                  color: featured ? "gold" : undefined,
+                }}
+              ></i>{" "}
+              Featured Speaker
+            </button>
           </span>
         </div>
       </div>
     </div>
-	);
+  ));
 };
 
 const SpeakerDetails = () => {
-
-    /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+  /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
   return (
-    <div key={'id'} className="col-xs-12" style={{ padding: 5 }}>
+    <div key={"id"} className="col-xs-12" style={{ padding: 5 }}>
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'name'}</h3>
+          <h3 className="panel-title">{"name"}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'bio'}</h5>
+          <h5>{"bio"}</h5>
         </div>
         <div className="panel-footer">
-          {{
-						/* ---> Loop through speaker's sessions here */
-					}}
+          {
+            {
+              /* ---> Loop through speaker's sessions here */
+            }
+          }
         </div>
       </div>
     </div>
@@ -84,7 +116,6 @@ export function Speaker() {
   );
 }
 
-
 export function Speakers() {
   return (
     <>
@@ -96,5 +127,3 @@ export function Speakers() {
     </>
   );
 }
-
-	
